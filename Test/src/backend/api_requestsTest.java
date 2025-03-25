6fb2891f4ccf14f13b05ffaa7ba458e1961138e4
Oterman2169 <o.samuels@unasat.sr>
2025-03-24 22:08:34 -0300

    Tests implemented

---
+++ Test/src/backend/api_requestsTest.java
@@ -0,0 +1,64 @@
+package backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class api_requestsTest {
+
+    @Test
+    void getStudents() {
+    }
+
+    @Test
+    void getCourses() {
+    }
+
+    @Test
+    void getCoursesNotInExams() {
+    }
+
+    @Test
+    void student_toevoegen() {
+    }
+
+    @Test
+    void exam_toevoegen() {
+    }
+
+    @Test
+    void student_bewerken() {
+    }
+
+    @Test
+    void cijfer_bewerken() {
+    }
+
+    @Test
+    void cijfer_verwijderen() {
+    }
+
+    @Test
+    void student_verwijderen() {
+    }
+
+    @Test
+    void getSemesters() {
+    }
+
+    @Test
+    void getExams() {
+    }
+
+    @Test
+    void getGradesForStudent() {
+    }
+
+    @Test
+    void testExam_toevoegen() {
+    }
+
+    @Test
+    void getGradesForExam() {
+    }
+}
