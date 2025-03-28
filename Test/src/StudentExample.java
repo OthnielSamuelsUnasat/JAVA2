9c0d88781465e9c9c7b7c458b801c278958678f9
Oterman2169 <o.samuels@unasat.sr>
2025-03-27 21:08:53 -0300

    Everything Working

--- Test/src/StudentExample.java
+++ Test/src/StudentExample.java
@@ -8,12 +8,10 @@ public class StudentExample {
 
         Student student = new Student(100, "Johnny", "Doe", "SE/1121/121", "M", "2000-01-01");
 
-
         student.setMajor("SE");
         student.setCohort(1101);
         student.setTotal_ec(120);
         student.setPassword("SecurePassword123");
-
 
         String response = api_requests.student_toevoegen(student);
 
